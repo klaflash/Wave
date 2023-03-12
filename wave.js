abbea77a386419a0ad516dde6f6a1882fdc97b76
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-03-12 06:04:58 +0000

    Updated geolocation

--- wave.js
+++ wave.js
@@ -1,5 +1,17 @@
+const locations = [{name:"test", latitude:26.775150, longitude:-80.033174}]
+const threshold = 0.1;
+
 const successCallback = (position) => {
     console.log(position);
+    const lat = pos.coords.latitude;
+    const lng = pos.coords.longitude;
+    const dist = distance(degToRad(lat), degToRad(lng), degToRad(locations[0].latitude), degToRad(locations[0].longitude))
+    console.log(dist)
+    if (dist < threshold) {
+        //full access
+    } else {
+        //restricted access
+    }
   };
   
   const errorCallback = (error) => {
@@ -12,8 +24,17 @@ const successCallback = (position) => {
   };
   
   const mediaQuery = window.matchMedia('(max-width: 480px)')
-  
-  if (mediaQuery.matches) {
-    navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+
+//   if (mediaQuery.matches) {
+//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+//   }
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+
+  const degToRad = (deg) => {
+    return deg * Math.PI / 180
+  }
+
+  const distance = (lat1, lon1, lat2, lon2) => {
+    return 6371 * Math.acos(Math.sin(lat1) * Math.sin(lat2) + Math.cos(lat1) * Math.cos(lat2) * Math.cos(lon2 - lon1))
   }
   

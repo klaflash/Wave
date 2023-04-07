f9013c6f781c7a1c9b7a8d5dafefa7c4a50e26a9
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-04-07 21:08:53 +0000

    Added log statement

--- location.js
+++ location.js
@@ -1,3 +1,4 @@
+console.log(currentLocation);
 if (!inRange[currentLocation.name]) {
 document.getElementById('rating').style.display = 'none'
 document.getElementById('range-message').textContent = 'To rate this location get within range and refresh'

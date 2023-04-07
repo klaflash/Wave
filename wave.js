39b02aa968c0af80ef5a0bee6dd6b1cdb0b43a36
Kyle Laflash <70404514+klaflash@users.noreply.github.com>
2023-04-07 21:53:02 +0000

    If i messed something up it was this commit

--- wave.js
+++ wave.js
@@ -2,61 +2,69 @@ const locations = [{name:"test", latitude:26.775044, longitude:-80.032890}, {nam
 const threshold = 0.05;
 let inRange = {test:false, hub:false}
 let currentLocation = 'what';
+let onMainPage = true;
 
 console.log(location.href)
 
-for (let location of locations) {
+if (location.href.includes('location.html')) {
+  onMainPage = false
+}
+
+if (onMainPage) {
+
+  for (let location of locations) {
     const node = document.createElement('button')
     node.textContent = `${location.name}`
     node.setAttribute('id', `${location.name}`)
     node.setAttribute('class', 'location')
     document.getElementById('locations').appendChild(node)
-}
+  }
 
-for (let location of locations) {
-  document.getElementById(`${location.name}`).addEventListener('click', () => {
-    window.location.href = './location.html'
-    currentLocation = location
-  })
-}
+  for (let location of locations) {
+    document.getElementById(`${location.name}`).addEventListener('click', () => {
+      window.location.href = './location.html'
+      currentLocation = location
+    })
+  }
 
-const successCallback = (position) => {
-    console.log(position);
-    const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
+  const successCallback = (position) => {
+      console.log(position);
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
 
-    for (let location of locations) {
-      const dist = distance(degToRad(lat), degToRad(lng), degToRad(location.latitude), degToRad(location.longitude))
-      console.log(dist)
-      if (dist < threshold) {
-          //full access
-          // const node = document.createElement('div')
-          // node.textContent = `Review ${location.name}`
-          // node.setAttribute('class', 'review')
-          // document.getElementById(`${location.name}`).appendChild(node)
-          inRange[location.name] = true;
-      } else {
-          //restricted access
+      for (let location of locations) {
+        const dist = distance(degToRad(lat), degToRad(lng), degToRad(location.latitude), degToRad(location.longitude))
+        console.log(dist)
+        if (dist < threshold) {
+            //full access
+            // const node = document.createElement('div')
+            // node.textContent = `Review ${location.name}`
+            // node.setAttribute('class', 'review')
+            // document.getElementById(`${location.name}`).appendChild(node)
+            inRange[location.name] = true;
+        } else {
+            //restricted access
+        }
       }
-    }
+      
+    };
     
-  };
-  
-  const errorCallback = (error) => {
-    console.log(error);
-  };
+    const errorCallback = (error) => {
+      console.log(error);
+    };
 
-  const options = {
-    enableHighAccuracy: true,
-    timeout: 10000,
-  };
-  
-  const mediaQuery = window.matchMedia('(max-width: 480px)')
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 10000,
+    };
+    
+    const mediaQuery = window.matchMedia('(max-width: 480px)')
+    //   if (mediaQuery.matches) {
+    //     navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+    //   }
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
 
-//   if (mediaQuery.matches) {
-//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
-//   }
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback, options);
+  }
 
   const degToRad = (deg) => {
     return deg * Math.PI / 180
